e3fbf457ed1ab3c2aebf404fc3535fc8d085f1ee
ahmedalimcs <ahmedalimcs21@gmail.com>
2021-10-25 15:08:29 +0500

    workcomplete

---
+++ app.js
@@ -0,0 +1,223 @@
+// upper case and lower case
+
+// var a= "Ahmed";
+// var b= a.toUpperCase(a);
+// alert(a);
+
+var a= "samina";
+   a= a.toUpperCase(a)
+   console.log(a)
+
+   var c="maryam";
+       c= c.toUpperCase(c)
+    console.log(c)
+
+    var x="AHMED";
+        x= x.toLowerCase(x)
+        console.log(x)
+
+    var x= "shandaar"
+    var b = prompt("enter your name")
+    if(x==b){
+        alert("same")
+    }
+
+    var b="for you";
+    var c= b.slice(0,7);
+    console.log(c)
+
+    var d="i like you";
+    var e= d.slice(0,8)
+    console.log(d)
+
+    var a= "basit"
+    var a1= a.slice(0,1)
+    a1= a1.toUpperCase()
+
+    var a2= a.slice(1)
+    a2= a2.toLowerCase()
+
+    var final=a1+a2
+    console.log(a1+a2)
+
+    // var pera=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur"
+
+    // var a="consectetur"
+    // var b="Dummy text"
+
+    // if(pera.slice (i= 0, i+11 )===a ){
+    //     alert("word found"){
+    //         else ("word not found")
+    //     }
+    var pera=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur"
+    var  index = pera.indexOf("adipisicing")
+    console.log(index)
+
+    var pera="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur officiis aperiam at iure maxime excepturi. Natus recusandae inventore quibusdam ratione vero dolores voluptate sit!"
+    var index= pera.replace("consectetur","syedahmedali")
+    console.log(index)
+
+
+    var pera="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur officiis aperiam at iure maxime excepturi. Natus recusandae inventore quibusdam ratione vero dolores voluptate sit!"
+    var index= pera.replace(/consectetur/g,"syedahmedali")
+    console.log(index)
+
+    var pera="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur officiis aperiam at iure maxime excepturi. Natus recusandae inventore quibusdam ratione vero dolores voluptate sit!"
+    var b= pera.charAt(30)
+    console.log(b)
+
+    var pera="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur officiis aperiam at iure maxime excepturi. Natus recusandae inventore quibusdam ratione vero dolores voluptate sit!"
+    var b= pera.indexOf("abcd")
+    console.log(b)
+
+    var pera="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat architecto quibusdam, sunt porro, ut consectetur officiis aperiam at iure maxime excepturi. Natus recusandae inventore quibusdam ratione vero dolores voluptate sit!"
+    var d= pera.lastIndexOf("voluptate sit")
+    console.log(d)
+
+    var x=["a", "b","c","d"]
+    var c="x"
+    if(x.indexOf(c)===-1){
+        alert(c)
+    }
+
+    // Random numbers
+    var a= Math.random()
+    console.log(a)
+
+    var a= 15.3;
+    var b= Math.floor(a);
+    console.log(b)
+
+    var a= 15.8;
+    var b= Math.ceil(a);
+    console.log(b)
+
+    // var a= 15.8;
+    // var b= b.tofixed(4,8);
+    // console.log(b)
+
+
+    var x= 600;
+    var b= 5;
+    var c= x*b;
+    document.write("total cost to buy 5 wicket 3000pkr");
+
+    // 5. Write a script to display multiplication table of any
+    // number in your browser. E.g
+
+    // var a= 4;
+    // var b= 1;
+
+    // var = prompt (("enter your 1st value"_);
+    // var = prompt ("enter your 2nd value");
+
+    //   var = prompt("enter your 1st value");
+
+
+    // for( i = 5; i < 11; i++){
+    //     document.write(a +"x" +i+ "=" +(a*i) ( "<br />")) 
+    // }
+         
+    // a = 5;   
+    // for(i = 0; i < 11; i++){
+    //   console.log("name 1" +"a")
+    // }
+
+    //  var a=prompt("enter your first value");
+    //  var b=prompt("enter your 2nd value");
+
+    //  for( i = 0; i < 11; i++){
+
+    //  }
+    //         document.write(a +"x" +i+ "=" +(a*i) + ( ("<br />")) 
+
+       
+    //         var a = "16";
+    //             console.log( typeof (+f) )
+    //             var a = "16";
+    //             console.log( typeof number(+f) )
+
+    //             var a = "16";
+    //             console.log( parseInt("a")) 
+
+    //             var x= "20";
+    //                 f=x.toString("20")
+    //             console.log(f)
+
+    //             var dicevalue= a*6
+    //             console.log(Math.ceil(dicevalue))
+    //             if(Math.ceil(dicevalue)===1){
+    //                 alert("tail")
+    //                 else{
+    //                     alert("had")
+    //                 }
+    //             }
+
+    //             var x = ("10");
+    //             var n = ("ali");
+    //             var c= (x+n);
+    //             console.log(c);
+
+                // var a= 9 % 3;
+                // console.log(a)
+
+
+                // var x=("you are so beautiful");
+                // if(x==="ahmed"){
+                //     alert("correct")
+                // }
+
+                // if(ticketnumber===3567){
+                //     alert("better luck")
+                // }
+
+                // if(ticketnumber!==47890){
+                //     alert("your good luck")
+                // }
+ 
+                // var c = new Date()
+                // console.log(c)
+
+                var a = new Date()
+                var allMiliseconds = a.getTime()
+                var myDOB = a.getTime ('Nov 04, 1992')
+                var ageMiliseconds = allMiliseconds - myDOB
+                console.log(ageMiliseconds)
+                var ageMiliseconds = ageMiliseconds/(1000*60*60*24*365)
+                console.log(ageMiliseconds)
+
+                var e = Math.floor("ageMiliseconds")
+                console.log(e)
+
+            // var a = new Date()
+            // console.log(a)
+
+            // var a = new Date()
+            // var allMiliseconds = a.getTime()
+            // var myDOB = ('Nov 04, 1992')
+            // console.log(myDOB)
+
+            // console.log(a.getTime())
+            // console.log(a.getDay())
+            
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+
+
+
+
